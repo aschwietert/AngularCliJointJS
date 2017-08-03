0c490036cb256473aa34104460512f8088505877
rekuzar <AlexSchwietert@gmx.net>
2017-08-03 20:33:31 +0200

    diagram-js

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-npm install lodash@3.10.1 --save-dev
-npm install backbone@1.3.3 --save-dev
-npm install jquery@3.1.1 --save-dev
-npm install jointjs@latest --save
-npm install @types/lodash@3.10.1 --save-dev
-npm install @types/jointjs --save-dev
+## JointJS
+
+* npm install lodash@3.10.1 --save-dev
+* npm install backbone@1.3.3 --save-dev
+* npm install jquery@3.1.1 --save-dev
+* npm install jointjs@latest --save
+* npm install @types/lodash@3.10.1 --save-dev
+* npm install @types/jointjs --save-dev
+
+
+## BPMN.io
+
+* npm install --save diagram-js
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef,ViewChild, AfterViewInit } from '@angular/core';
 import * as jQuery from 'jquery';
 import * as _ from 'lodash';
 import * as $ from 'backbone';
 import * as joint from 'jointjs';
-
+import * as Diagram from 'diagram-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'app';
+  @ViewChild('diagram1') diagram1: ElementRef;
+  @ViewChild('diagram2') diagram2: ElementRef;
+  @ViewChild('diagram3') diagram3: ElementRef;
 
 
   ngOnInit() {
@@ -41,6 +44,42 @@ export class AppComponent implements OnInit {
     });
 
      graph.addCells([rect, rect2, link]);
+
+    
+
+  }
+
+  ngAfterViewInit() {
+
+
+    var diagram1 = new Diagram({ canvas: { container: this.diagram1.nativeElement }});
+
+    var diagram2 = new Diagram({
+      canvas: { container: document.getElementById('diagram-2') },
+      components: [ 'selectionVisuals' ]
+    });
+
+    var diagram3 = new Diagram({
+      canvas: { container: document.getElementById('diagram-3') },
+      modules: {
+        'renderer': [ 'type', function TrafficLightRenderer() {
+          this.drawShape = function(paper, data) {
+            var element = paper.rect(0, 0, data.width, data.height);
+            element.attr('fill', data.type || '#CCC');
+            return element;
+          };
+          this.drawConnection = function(paper, data) {
+            return paper.rect(10, 10, 100, 100);
+          }
+        }]
+      }
+    });
+
+        diagram1.get('canvas').addShape({ id: 's1', type: 'red', x: 10, y: 20, width: 100, height: 100 });
+          diagram2.get('canvas').addShape({ id: 's2', type: 'green', x: 10, y: 140, width: 100, height: 100 });
+            diagram3.get('canvas').addShape({ id: 's3', type: 'yellow', x: 10, y: 260, width: 100, height: 100 });
+
+
 
 
 
